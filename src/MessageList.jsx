b8c120f666a663ae09e45c31b23eefe32cd3b87e
Christina <christina.hsu.ca@gmail.com>
2019-02-12 03:50:46 +0000

    add basic rendering and jsx files

---
+++ src/MessageList.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div>
+      <main className="messages">
+        <Message />
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
+      </div>
+
+    );
+  }
+}
+export default MessageList;
